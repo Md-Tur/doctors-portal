84a66b94ec94a990b085184b980990d8437c5c67
Md-Tur <turibnmostafiz@gmail.com>
2022-05-10 07:33:31 +0600

    homepage updated

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,9 +7,9 @@ import phone from '../../assets/icons/phone.svg';
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard bgclassName="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening Hours" img={clock}></InfoCard>
-            <InfoCard bgclassName="bg-accent" cardTitle="Visit Our Location" img={marker}></InfoCard>
-            <InfoCard bgclassName="bg-gradient-to-r from-secondary to-primary" cardTitle="Contact Us Now" img={phone}></InfoCard>
+            <InfoCard bgclass="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening Hours" img={clock}></InfoCard>
+            <InfoCard bgclass="bg-accent" cardTitle="Visit Our Location" img={marker}></InfoCard>
+            <InfoCard bgclass="bg-gradient-to-r from-secondary to-primary" cardTitle="Contact Us Now" img={phone}></InfoCard>
         </div>
     );
 };
