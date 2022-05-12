318ee26c2603bf6cd1cbce2edea09bd3143326b7
Md-Tur <turibnmostafiz@gmail.com>
2022-05-12 21:48:28 +0930

    login & sign out implemented

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,35 +1,42 @@
 import React from 'react';
-import { useSignInWithGoogle, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSignInWithGoogle, useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { async } from '@firebase/util';
 
-const Login = () => {
+const SignUp = () => {
 
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
 
-    const onSubmit = data => {
-        console.log(data)
-        signInWithEmailAndPassword(data.email, data.password);
-    };
-
-    let signInError;
-
     const [
-        signInWithEmailAndPassword,
+        createUserWithEmailAndPassword,
         user,
         loading,
         error,
-    ] = useSignInWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth);
 
-    if (gLoading || loading) {
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const navigate = useNavigate();
+
+    let signInError;
+
+    const onSubmit = async data => {
+        await createUserWithEmailAndPassword(data.email, data.password);
+        await updateProfile({ displayName: data.name });
+        console.log('Profile Updated');
+        navigate('/appoinment');
+    };
+
+    if (gLoading || loading || updating) {
         return <Loading></Loading>
     }
 
-    if (gError || error) {
-        signInError = <p className='text-red-500'><small>{gError?.message || error?.message}</small></p>
+    if (gError || error || updateError) {
+        signInError = <p className='text-red-500'><small>{gError?.message || error?.message || updateError?.message}</small></p>
     }
 
     if (gUser || user) {
@@ -40,8 +47,27 @@ const Login = () => {
         <div className='flex h-screen justify-center items-center'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <h2 className="text-center text-2xl font-bold">Sign Up</h2>
                     <form onSubmit={handleSubmit(onSubmit)}>
+
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Your Name"
+                                className="input input-bordered w-full max-w-xs"
+                                {...register("name", {
+                                    required: {
+                                        value: true,
+                                        message: "Name is Required"
+                                    }
+                                })} />
+                            <label className="label">
+                                {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                            </label>
+                        </div>
 
                         <div className="form-control w-full max-w-xs">
                             <label className="label">
@@ -91,9 +117,9 @@ const Login = () => {
                             </label>
                         </div>
                         {signInError}
-                        <input className='btn w-full max-w-xs' value="Login" type="submit" />
+                        <input className='btn w-full max-w-xs' value="Sign up" type="submit" />
                     </form>
-                    <p><small>New to Doctors Portal? <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
+                    <p><small>Already have an account? <Link className='text-primary' to="/login">Please Login here</Link></small></p>
                     <div className="divider">OR</div>
                     <button
                         onClick={() => signInWithGoogle()}
@@ -106,4 +132,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyC6tRn5yWtWWOlzK7Rgk_V30viqkONWhzo",
+    authDomain: "doctors-portal-ae7c9.firebaseapp.com",
+    projectId: "doctors-portal-ae7c9",
+    storageBucket: "doctors-portal-ae7c9.appspot.com",
+    messagingSenderId: "51106843546",
+    appId: "1:51106843546:web:d96ea63028ea2ba1978823"
 };
 
 // Initialize Firebase
